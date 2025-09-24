e6c14d965cfbf6b810a8de5564123d35addc5620
DataSoftwarePrecimec <sistemas@quirurgicos.co>
2025-09-24 16:58:32 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+const GAS_URL = "https://script.google.com/macros/s/AKfycbw53IZjag5cbwGOq0qTwnnv_w3iD00hDD8gjHnQBUfa1e37scVUXW97JQpnjtM_WgLW/exec"
+
 function initApp() {
   fetch("/get_rows")
     .then(res => {
