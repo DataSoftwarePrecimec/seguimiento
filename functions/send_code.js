2395c9a189cc69b1fbc307a17fa40a354f289a05
DataSoftwarePrecimec <sistemas@quirurgicos.co>
2025-09-25 14:13:13 -0500

    Update send_code.js

--- functions/send_code.js
+++ functions/send_code.js
@@ -1,9 +1,9 @@
 export async function onRequest(context) {
   try {
-    let body = await context.request.json();
-    session_code      = crypto.randomUUID();
-    body.session_code = session_code;
-    const url = "https://script.google.com/macros/s/AKfycbxbsgfFR49j44PFsXi-BlxiD-0snFJaZU40kUOe0GcAmYKn7d8KcH3qQWVuG8g6jl7N/exec";
+    let body                = await context.request.json();
+    globalThis.session_code = crypto.randomUUID();
+    body.session_code       = session_code;
+    const url               = "https://script.google.com/macros/s/AKfycbxbsgfFR49j44PFsXi-BlxiD-0snFJaZU40kUOe0GcAmYKn7d8KcH3qQWVuG8g6jl7N/exec";
     const response = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
