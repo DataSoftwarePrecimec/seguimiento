f2ca46c5e420a652ad8fc0a566d81fc5b1c94e85
DataSoftwarePrecimec <sistemas@quirurgicos.co>
2025-09-26 07:38:02 -0500

    Update download_report.js

--- functions/download_report.js
+++ functions/download_report.js
@@ -15,9 +15,6 @@ export async function onRequestPost(context) {
       }
     );
     const data = await response.json();
-    for (let key in data){
-      console.log(key + ': ' + data[key]);
-    }
     return new Response(JSON.stringify(data), {
       status: response.status,
       headers: {
